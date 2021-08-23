49e70aa815aed2fe276e14c7694e4eb4ff0ed7e4
patjawat <patjawat@gmail.com>
2021-08-23 13:14:04 +0700

    add

--- themes/adminlte3/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://lawdivision.kku.ac.th">กองทรัพยากรบุคคล มหาวิทยาลัยขอนแก่น</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1
-    </div>
-</footer>
--- views/settings/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$this->title = 'ตั้งค่าระบบ';
-?>
-
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
